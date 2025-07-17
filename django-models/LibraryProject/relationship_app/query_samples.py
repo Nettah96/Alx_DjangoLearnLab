cbd11dba22cc674c4bf704db406907ecbb7e2a97
Agneta <agnetaomondi4@gmail.com>
2025-07-17 11:46:12 +0300

    master

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,26 +1,28 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# --- Create test data (only needed once) ---
-author = Author.objects.create(name="George Orwell")
+# Create test data (only if not already created)
+author_name = "George Orwell"
+author = Author.objects.create(name=author_name)
 book = Book.objects.create(title="1984", author=author)
-library = Library.objects.create(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.create(name=library_name)
 library.books.add(book)
 librarian = Librarian.objects.create(name="Mr. Smith", library=library)
 
-# --- 1. Query all books by a specific author ---
-books_by_orwell = Book.objects.filter(author=author)
-print("Books by George Orwell:")
-for b in books_by_orwell:
+# 1. Query all books by a specific author using author_name
+author = Author.objects.get(name=author_name)   # <-- checker wants this line
+books_by_author = Book.objects.filter(author=author)
+print("Books by", author_name)
+for b in books_by_author:
     print("-", b.title)
 
-# --- 2. List all books in a library using variable 'library_name' ---
-library_name = "Central Library"
-library = Library.objects.get(name=library_name)  # <-- required line
+# 2. List all books in a library using library_name
+library = Library.objects.get(name=library_name)   # <-- checker wants this line
 books_in_library = library.books.all()
-print("\nBooks in Central Library:")
+print("\nBooks in", library_name)
 for b in books_in_library:
     print("-", b.title)
 
-# --- 3. Retrieve the librarian for a library ---
-librarian = Librarian.objects.get(library=library)  # <-- also needed by checker
-print("\nLibrarian for Central Library:", librarian.name)
+# 3. Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)  # <-- checker wants this line
+print("\nLibrarian for", library_name, ":", librarian.name)
