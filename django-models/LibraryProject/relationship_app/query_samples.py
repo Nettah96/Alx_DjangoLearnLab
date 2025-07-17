5917015c767d067082998c585eca13c6451435e4
Agneta <agnetaomondi4@gmail.com>
2025-07-17 11:32:13 +0300

    master

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,21 @@
+import django
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+django.setup()
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+author = Author.objects.get(name="Jane Austen")
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+
+# List all books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
+print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
+
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print(f"Librarian at {library.name}: {librarian.name}")
