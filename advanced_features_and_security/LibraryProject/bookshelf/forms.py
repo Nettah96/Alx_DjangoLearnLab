de51caf6c193ea349786728b11e97c60a7b7727e
Agneta <agnetaomondi4@gmail.com>
2025-07-26 00:54:10 +0300

    master

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,20 +1,6 @@
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'description']  # Change these fields to match your Book model
-
-        # Optional: Add widgets or labels for better UX and security
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
-        }
-
-        labels = {
-            'title': 'Book Title',
-            'author': 'Author Name',
-            'description': 'Short Description',
-        }
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
