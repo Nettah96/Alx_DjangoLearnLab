9ce733ac5fb0c9475a7b8137bba9eaf5186aab44
Agneta <agnetaomondi4@gmail.com>
2025-08-10 01:16:20 +0300

    master

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+
 
 # List all books - open to all users (read-only)
 class BookListView(generics.ListAPIView):
