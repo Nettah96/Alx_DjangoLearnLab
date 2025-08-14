f8731ff71de9a1d5e9a8e1f5d07c1200f130e247
Agneta <agnetaomondi4@gmail.com>
2025-08-14 20:53:38 +0300

    master

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,12 +3,16 @@ from .models import Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
-from .models import Tag
+from taggit.forms import TagWidget
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'tags']  # include tags in the form
+        widgets = {
+            'tags': TagWidget(),  
+        }
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -37,10 +41,4 @@ class PostForm(forms.ModelForm):
         instance = super().save(commit=False)
         if commit:
             instance.save()
-        # Process tags
-        tags_str = self.cleaned_data.get('tags', '')
-        tag_names = [t.strip() for t in tags_str.split(',') if t.strip()]
-        for name in tag_names:
-            tag, created = Tag.objects.get_or_create(name=name)
-            instance.tags.add(tag)
-        return instance
+
